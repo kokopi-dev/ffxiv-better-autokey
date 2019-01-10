a10bfdd82ed60513667ad13d1d7a5de626ae0364
Derrick <km.commissions@gmail.com>
2019-01-10 00:05:43 -0800

    added key update feature

--- auto-craft.py
+++ auto-craft.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 """
-DEBUGGING: CHECK COMMENT BLOCKS BELOW AND MAKE SURE INPUTS ARE CORRECT
 Use the example to calculate sleep time:
 YOUR_MACRO_WAIT_TOTAL + 5
 
-Example:
-time.sleep(1)
-MACRO_1
-time.sleep(39)
-MACRO_2
-...
+You can manually edit the timers and keystrokes
+in the .json file
 """
 import json
 import os
@@ -20,17 +15,23 @@ import time
 from pywinauto.application import Application
 from pywinauto.keyboard import *
 
-
-# Intro
+#############
+### Intro ###
+#############
 current_path = os.path.dirname(os.path.abspath(__file__))
 # Opening json data file to read
 with open(("%s/ffxiv-autocraft_data.json" % current_path), "r") as time_data:
 	macro_time = json.load(time_data)
-# Dsiplaying current macro timers
+# Displaying current macro timers
 m_list = [macro_time["m1"], macro_time["m2"], macro_time["m3"], macro_time["m4"]]
 print("The current macro timers are:\n")
-print("M1\tM2\tM3\tM4")
+print("m1\tm2\tm3\tm4")
 print(*m_list, sep='\t')
+# Displaying current keystrokes
+k_list = [macro_time["k1"], macro_time["k2"], macro_time["k3"], macro_time["k4"]]
+print("\nThe current keystrokes are:\n")
+print("k1\tk2\tk3\tk4")
+print(*k_list, sep='\t')
 
 # argc, argv
 argv = sys.argv[1:]
@@ -41,24 +42,37 @@ if argc > 1:
 	print("  <Error: Too many arguments (must be one).>")
 	sys.exit()
 
-if argc == 1 and sys.argv[1] != "edit":
+if argc == 1 and sys.argv[1] != "edit" and sys.argv[1] != "editkeys":
 	print("  <Error: %s command not found.>" % sys.argv[1])
 
 editor = ""
-editedCount = 0
-# Editing macro timers
+editedTimerCount = 0
+editedKeyCount = 0
 try:
+	# EDITING MACRO TIMERS
 	if sys.argv[1] == "edit":
 		editor = "y"
 		while editor == "y":
 			print("\nFormat example: m1 35")
 			print("Which macro timer do you want to edit?")
+			# User input
 			editThis = input()
-		
-			if editThis == "":
-				print("  <Error: input cannot be none, try again.>")
-				sys.exit()
+			print()
 
+			try:
+				if editThis == "":
+					print("  <Error: input cannot be none, try again.>")
+					sys.exit()
+				if editThis[2] != ' ':
+					print("Wrong format.\nFormat example: m1 35")
+					sys.exit()
+				if len(editThis) < 3:
+					print("Wrong format.\nFormat example: k1 3")
+					sys.exit()
+			except IndexError:
+				print("Wrong format.\nFormat example: k1 3")
+				sys.exit()
+			# Splitting input into macro and macro timer
 			try:
 				mtime = int(editThis[3:])
 				m = editThis[:2]
@@ -66,9 +80,6 @@ try:
 				print("Wrong format.\nFormat example: m1 35")
 				sys.exit()
 
-			if editThis[2] != ' ':
-				print("Wrong format.\nFormat example: m1 35")
-				sys.exit()
 			if m[0] != 'm':
 				print("Wrong format.\nFormat example: m1 35")
 				sys.exit()
@@ -92,28 +103,111 @@ try:
 				print("  ... saving data...rereading data...done.")
 
 			# Increment times edited
-			editedCount += 1
+			editedTimerCount += 1
 
-			# continue or exit
+			# Continue or exit
 			print("\nDo you want to edit another macro timer? (y/n)")
 			editor = input()
+			print()
 			if editor == "y":
 				pass
 			elif editor == "n":
 				print("  ... Program will continue to auto craft")
 				break
 			else:
-				print(" <Error: input needs to be 'y' or 'n'.>")
+				print(" <Error: input needs to be 'y' or 'n', exiting program.>")
+				sys.exit()
+	# EDITING KEYS
+	elif sys.argv[1] == "editkeys":
+		editor = "y"
+		while editor == "y":
+			acceptedKeys = ["1", "2", "3", "4", "5", "6", "7",
+							"8", "9", "0", "-", "="]
+			acceptedPlace = ["k1", "k2", "k3", "k4"]
+
+			print("\nAccepted key edits are the following:\n")
+			print(' '.join(map(str, acceptedKeys)))
+			print("\nFormat example: k1 3")
+			print("Which keystroke do you want to edit?")
+			# User input
+			editThis = input()
+			print()
+
+			try:
+				if editThis == "":
+					print("  <Error: input cannot be none, try again.>")
+					sys.exit()
+				if editThis[2] != ' ':
+					print("Wrong format.\nFormat example: k1 3")
+					sys.exit()
+				if len(editThis) < 3:
+					print("Wrong format.\nFormat example: k1 3")
+					sys.exit()
+			except IndexError:
+				print("Wrong format.\nFormat example: k1 3")
+				sys.exit()
+
+			# Splitting input into keys and keystrokes
+			try:
+				kstroke = str(editThis[3:])
+				k = str(editThis[:2])
+			except:
+				print("Wrong format.\nFormat example: k1 3")
+				sys.exit()
+
+			if k not in acceptedPlace:
+				print("Wrong format.\nFormat example: k1 3")
+				sys.exit()
+			if kstroke not in acceptedKeys:
+				print("Keystroke not accepted, check accepted key edits above.")
+				sys.exit()
+
+			# Temp adding to json
+			with open("%s/ffxiv-autocraft_data.json" % current_path) as add_key_data:
+				add_key = json.load(add_key_data)
+
+			print("  ... editing requested keystrokes.")
+			add_key[k] = kstroke
+
+			# Saving to json
+			with open(("%s/ffxiv-autocraft_data.json" % current_path), "w") as save_key_data:
+				json.dump(add_key, save_key_data)
+			# Rereading json
+			with open(("%s/ffxiv-autocraft_data.json" % current_path), "r") as time_data:
+				macro_time = json.load(time_data)
+				print("  ... saving data...rereading data...done.")
+
+			# Increment times edited
+			editedKeyCount += 1
+
+			# Continue or exit
+			print("\nDo you want to edit another keystroke? (y/n)")
+			editor = input()
+			print()
+			if editor == "y":
+				pass
+			elif editor == "n":
+				print("  ... Program will continue to auto craft")
+				break
+			else:
+				print(" <Error: input needs to be 'y' or 'n', exiting program.>")
 				sys.exit()
 except IndexError:
 	pass
 
 # Displaying new timers if edited
-if editedCount > 0:
+if editedTimerCount > 0:
 	m_list = [macro_time["m1"], macro_time["m2"], macro_time["m3"], macro_time["m4"]]
 	print("\nThe new macro timers are:\n")
-	print("M1\tM2\tM3\tM4")
+	print("m1\tm2\tm3\tm4")
 	print(*m_list, sep='\t')
+
+# Displaying new timers if edited
+if editedKeyCount > 0:
+	k_list = [macro_time["k1"], macro_time["k2"], macro_time["k3"], macro_time["k4"]]
+	print("\nThe new keystrokes are:\n")
+	print("k1\tk2\tk3\tk4")
+	print(*k_list, sep='\t')
 
 # Task Manager -> Right click FFXIV -> Go to details
 process_name = "ffxiv_dx11.exe"
@@ -135,7 +229,7 @@ app = Application().connect(process=ffxiv_pid)
 print('\nPress Ctrl-C to quit crafting.')
 try:
 	while True:
-		time.sleep(2)
+		time.sleep(3)
 		# SELECT "SYNTHESIZE"
 		app.window(title='FINAL FANTASY XIV').send_keystrokes('{VK_NUMPAD0}')
 		print("  ... Selecting the button 'Synthesize'.")
@@ -145,11 +239,11 @@ try:
 		print("  ... Pressing the button 'Synthesize'.")
 		time.sleep(3)
 		# CRAFTING MACRO 1
-		app.window(title='FINAL FANTASY XIV').send_keystrokes('1')
+		app.window(title='FINAL FANTASY XIV').send_keystrokes(macro_time["k1"])
 		print("  ... Pressing macro 1 ... <wait.{:d}>.".format(macro_time["m1"]))
 		time.sleep(macro_time["m1"])
 		# CRAFTING MACRO 2
-		app.window(title='FINAL FANTASY XIV').send_keystrokes('2')
+		app.window(title='FINAL FANTASY XIV').send_keystrokes(macro_time["k2"])
 		print("  ... Pressing macro 2 ... <wait.{:d}>.".format(macro_time["m2"]))
 		time.sleep(macro_time["m2"])
 except KeyboardInterrupt:
