3fdaa93e835055d6c513452899f843c1f66651af
Derrick <km.commissions@gmail.com>
2019-01-11 23:18:06 -0800

    added autobuff feature

--- ffxiv-autocraft.py
+++ ffxiv-autocraft.py
@@ -21,14 +21,14 @@ from pywinauto.keyboard import *
 current_path = os.path.dirname(os.path.abspath(__file__))
 # Opening json data file to read
 with open(("%s/ffxiv-autocraft_data.json" % current_path), "r") as time_data:
-    macro_time = json.load(time_data)
+    json_data = json.load(time_data)
 # Displaying current macro timers
-m_list = [macro_time["m1"], macro_time["m2"], macro_time["m3"], macro_time["m4"]]
+m_list = [json_data["m1"], json_data["m2"], json_data["m3"], json_data["m4"]]
 print("The current macro timers are:\n")
 print("m1\tm2\tm3\tm4")
 print(*m_list, sep='\t')
 # Displaying current keystrokes
-k_list = [macro_time["k1"], macro_time["k2"], macro_time["k3"], macro_time["k4"]]
+k_list = [json_data["k1"], json_data["k2"], json_data["k3"], json_data["k4"]]
 print("\nThe current keystrokes are:\n")
 print("k1\tk2\tk3\tk4")
 print(*k_list, sep='\t')
@@ -38,11 +38,12 @@ argv = sys.argv[1:]
 argc = len(argv)
 
 # argument checker
+acceptedArguments = ["edit", "editkeys", "autobuff"]
 if argc > 1:
     print("  <Error: Too many arguments (must be one).>")
     sys.exit()
 
-if argc == 1 and sys.argv[1] != "edit" and sys.argv[1] != "editkeys":
+if argc == 1 and sys.argv[1] not in acceptedArguments:
     print("  <Error: %s command not found.>" % sys.argv[1])
 
 editor = ""
@@ -99,7 +100,7 @@ try:
                 json.dump(add_time, save_time_data)
             # Rereading json
             with open(("%s/ffxiv-autocraft_data.json" % current_path), "r") as time_data:
-                macro_time = json.load(time_data)
+                json_data = json.load(time_data)
                 print("  ... saving data...rereading data...done.")
 
             # Increment times edited
@@ -118,12 +119,12 @@ try:
                 print(" <Error: input needs to be 'y' or 'n', exiting program.>")
                 sys.exit()
     # EDITING KEYS
-    elif sys.argv[1] == "editkeys":
+    if sys.argv[1] == "editkeys":
         editor = "y"
         while editor == "y":
             acceptedKeys = ["1", "2", "3", "4", "5", "6", "7",
                             "8", "9", "0", "-", "="]
-            acceptedPlace = ["k1", "k2", "k3", "k4"]
+            acceptedPlace = ["k1", "k2", "k3", "k4", "k5"]
 
             print("\nAccepted key edits are the following:\n")
             print(' '.join(map(str, acceptedKeys)))
@@ -174,7 +175,7 @@ try:
                 json.dump(add_key, save_key_data)
             # Rereading json
             with open(("%s/ffxiv-autocraft_data.json" % current_path), "r") as time_data:
-                macro_time = json.load(time_data)
+                json_data = json.load(time_data)
                 print("  ... saving data...rereading data...done.")
 
             # Increment times edited
@@ -192,19 +193,143 @@ try:
             else:
                 print(" <Error: input needs to be 'y' or 'n', exiting program.>")
                 sys.exit()
+    # FOOD POT OPTION
+    if sys.argv[1] == "autobuff":
+        editor = "y"
+        while editor == "y":
+            # Food/pot tracking variables
+            print("Enter your current food buff timer in minutes:")
+            foodBuff = input()
+            while not isinstance(foodBuff, int):
+                try:
+                    foodBuff = int(foodBuff)
+                except ValueError:
+                    print("Food buff timer must be an integer. Please try again:")
+                    foodBuff = input()
+
+            while not 3 <= foodBuff <= 60:
+                print("Food buff timer needs to be an integer between 3 to 60. Please try again:")
+                foodBuff = input()
+                foodBuff = int(foodBuff)
+
+            print("Enter your current pot timer in minutes:")
+            potBuff = input()
+            while not isinstance(potBuff, int):
+                try:
+                    potBuff = int(potBuff)
+                except ValueError:
+                    print("Pot buff timer must be an integer. Please try again:")
+                    foodBuff = input()
+            while not 3 <= foodBuff <= 15:
+                print("Pot buff timer needs to be between 3 to 15. Please try again:")
+                foodBuff = input()
+
+            foodBuffSeconds = (foodBuff * 60) - 40
+            potBuffSeconds = (potBuff * 60) - 40
+
+            # Task Manager -> Right click FFXIV -> Go to details
+            process_name = "ffxiv_dx11.exe"
+
+            # AUTO PID
+            for proc in psutil.process_iter():
+                if proc.name() == process_name:
+                    findingPID = re.search('pid=(.+?), name=', str(proc))
+                    ffxiv_pid = int(findingPID.group(1))
+
+            # If auto PID doesnt work, check if the 'process_name' is correct in your task manager`
+            try:
+                app = Application().connect(process=ffxiv_pid)
+            except NameError:
+                print("  <Error: Process not found.>\n  ... Either make sure FFXIV is running or change 'process_name'")
+                sys.exit()
+
+            # Setup sleep times accordingly, and your keystrokes
+            print('\nPress Ctrl-C to quit crafting.')
+            try:
+                food_loss = 0
+                pot_loss = 0
+                while True:
+                    time.sleep(3)
+                    food_loss += 3
+                    pot_loss += 3
+                    if pot_loss >= foodBuffSeconds:
+                        time.sleep(1)
+                        app.window(title='FINAL FANTASY XIV').send_keystrokes('{VK_ESCAPE}')
+                        print("  ... STANDING UP BY EXITING")
+                        time.sleep(4)
+                        # POT KEY
+                        app.window(title='FINAL FANTASY XIV').send_keystrokes(json_data["k3"])
+                        print("  ... EATING FOOD")
+                        time.sleep(2)
+                        # CRAFT ITEM BUTTON
+                        app.window(title='FINAL FANTASY XIV').send_keystrokes(json_data["k5"])
+                        print("  ... SELECTING CRAFT")
+                        time.sleep(3)
+                        app.window(title='FINAL FANTASY XIV').send_keystrokes('{VK_NUMPAD0}')
+                        print("  ... HIGHLIGHT SELECT CRAFT")
+                        time.sleep(2)
+                        pot_loss = 0
+                        # default 15 min pot buff minus 30 seconds
+                        foodBuffSeconds = 870
+                    if food_loss >= foodBuffSeconds:
+                        time.sleep(1)
+                        app.window(title='FINAL FANTASY XIV').send_keystrokes('{VK_ESCAPE}')
+                        print("  ... STANDING UP BY EXITING")
+                        time.sleep(4)
+                        # FOOD KEY
+                        app.window(title='FINAL FANTASY XIV').send_keystrokes(json_data["k4"])
+                        print("  ... EATING FOOD")
+                        time.sleep(2)
+                        # CRAFT ITEM BUTTON
+                        app.window(title='FINAL FANTASY XIV').send_keystrokes(json_data["k5"])
+                        print("  ... SELECTING CRAFT")
+                        time.sleep(3)
+                        app.window(title='FINAL FANTASY XIV').send_keystrokes('{VK_NUMPAD0}')
+                        print("  ... HIGHLIGHT SELECT CRAFT")
+                        time.sleep(2)
+                        pot_loss = 0
+                        # default 30 min pot buff minus 30 seconds
+                        foodBuffSeconds = 1770
+                    # SELECT "SYNTHESIZE"
+                    app.window(title='FINAL FANTASY XIV').send_keystrokes('{VK_NUMPAD0}')
+                    print("  ... Selecting the button 'Synthesize'.")
+                    time.sleep(3)
+                    food_loss += 3
+                    pot_loss += 3
+                    # PRESS "SYNTHESIZE"
+                    app.window(title='FINAL FANTASY XIV').send_keystrokes('{VK_NUMPAD0}')
+                    print("  ... Pressing the button 'Synthesize'.")
+                    time.sleep(3)
+                    food_loss += 3
+                    pot_loss += 3
+                    # CRAFTING MACRO 1
+                    app.window(title='FINAL FANTASY XIV').send_keystrokes(json_data["k1"])
+                    print("  ... Pressing macro 1 ... <wait.{:d}>.".format(json_data["m1"]))
+                    time.sleep(json_data["m1"])
+                    food_loss += json_data["m1"]
+                    pot_loss += json_data["m1"]
+                    # CRAFTING MACRO 2
+                    app.window(title='FINAL FANTASY XIV').send_keystrokes(json_data["k2"])
+                    print("  ... Pressing macro 2 ... <wait.{:d}>.".format(json_data["m2"]))
+                    time.sleep(json_data["m2"])
+                    food_loss += json_data["m2"]
+                    pot_loss += json_data["m2"]
+            except KeyboardInterrupt:
+                print("Program has stopped.")
+                sys.exit()
 except IndexError:
     pass
 
 # Displaying new timers if edited
 if editedTimerCount > 0:
-    m_list = [macro_time["m1"], macro_time["m2"], macro_time["m3"], macro_time["m4"]]
+    m_list = [json_data["m1"], json_data["m2"], json_data["m3"], json_data["m4"]]
     print("\nThe new macro timers are:\n")
     print("m1\tm2\tm3\tm4")
     print(*m_list, sep='\t')
 
 # Displaying new timers if edited
 if editedKeyCount > 0:
-    k_list = [macro_time["k1"], macro_time["k2"], macro_time["k3"], macro_time["k4"]]
+    k_list = [json_data["k1"], json_data["k2"], json_data["k3"], json_data["k4"]]
     print("\nThe new keystrokes are:\n")
     print("k1\tk2\tk3\tk4")
     print(*k_list, sep='\t')
@@ -217,6 +342,7 @@ for proc in psutil.process_iter():
     if proc.name() == process_name:
         findingPID = re.search('pid=(.+?), name=', str(proc))
         ffxiv_pid = int(findingPID.group(1))
+        
 
 # If auto PID doesnt work, check if the 'process_name' is correct in your task manager`
 try:
@@ -239,12 +365,13 @@ try:
         print("  ... Pressing the button 'Synthesize'.")
         time.sleep(3)
         # CRAFTING MACRO 1
-        app.window(title='FINAL FANTASY XIV').send_keystrokes(macro_time["k1"])
-        print("  ... Pressing macro 1 ... <wait.{:d}>.".format(macro_time["m1"]))
-        time.sleep(macro_time["m1"])
+        app.window(title='FINAL FANTASY XIV').send_keystrokes(json_data["k1"])
+        print("  ... Pressing macro 1 ... <wait.{:d}>.".format(json_data["m1"]))
+        time.sleep(json_data["m1"])
         # CRAFTING MACRO 2
-        app.window(title='FINAL FANTASY XIV').send_keystrokes(macro_time["k2"])
-        print("  ... Pressing macro 2 ... <wait.{:d}>.".format(macro_time["m2"]))
-        time.sleep(macro_time["m2"])
+        app.window(title='FINAL FANTASY XIV').send_keystrokes(json_data["k2"])
+        print("  ... Pressing macro 2 ... <wait.{:d}>.".format(json_data["m2"]))
+        time.sleep(json_data["m2"])
 except KeyboardInterrupt:
     print("Program has stopped.")
+    sys.exit()
