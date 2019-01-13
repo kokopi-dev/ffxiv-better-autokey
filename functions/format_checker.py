05c47078aa4598e2c95c695fced362f3ec6bb56d
Derrick <km.commissions@gmail.com>
2019-01-13 12:54:34 -0800

    refactored method autobuff_checker

--- functions/format_checker.py
+++ functions/format_checker.py
@@ -12,8 +12,9 @@ class FormatCheck:
         self.user_input = user_input
 
     def timer_format(user_input):
-        """ Checks user input's format, returns if format is ok
+        """ Checks user input's format, returns format if ok
             Timer must be pos int, macros must be in acceptedHotkeys
+            Loops input() until correct format is given
         """
 
         acceptedHotkeys = ["m1", "m2", "m3", "m4"]
@@ -26,27 +27,26 @@ class FormatCheck:
             int_lock += 1
 
         while int_lock == 1:
-            print("Wrong timer data type.\nFormat example: m1 35\n")
+            print("Please re-enter in the correct format:")
             user_input = input()
             try:
                 temp_timer = int(user_input[3:])
                 while user_input[:2] not in acceptedHotkeys:
                     print("\nAccepted key edits are the following:")
                     print(' '.join(map(str, acceptedHotkeys)) + '\n')
-                    user_input = input()
                 while not temp_timer in range(0, 301):
-                    print("Wrong timer range, must be between 0-300.")
-                    user_input = input()
+                    print("  ... Wrong timer range, must be between 0-300.")
                 int_lock += 1
             except ValueError:
-                print("Wrong timer data type.\nFormat example: m1 35\n")
+                print("  ... Wrong timer data type.\n  ... Format example: m1 35\n")
 
         return user_input
 
     def keystroke_checker(user_input):
-        """ Checks user input's format, returns if format is ok
+        """ Checks user input's format, returns format if ok
             Keys must be in acceptedKeys
             Key placement must be in acceptedKeyplace
+            Loops input() until correct format is given
         """
         
         acceptedKeys = ["1", "2", "3", "4", "5", "6", "7",
@@ -59,6 +59,57 @@ class FormatCheck:
             print("\nAccepted key edits are the following:\n")
             print(' '.join(map(str, acceptedKeys)) + '\n')
             user_input = input()
+
+        return user_input
+
+    def autobuff_checker(user_input):
+        """ Checks user input's format, returns format if ok
+            Food timer must be between 3 and 60
+            Pot timer must be between 3 and 15
+            Loops input() until correct format is given
+        """
+
+        food_lock = 0
+        pot_lock = 0
+        
+        try:
+            temp_food = int(user_input[0])
+            if not temp_food in range(3, 61):
+                raise ValueError
+        except ValueError:
+            food_lock += 1
+            pass
+
+        try:
+            temp_pot = int(user_input[1])
+            if not temp_pot in range(3, 16):
+                raise ValueError
+        except ValueError:
+            pot_lock += 1
+            pass
+
+        while food_lock == 1:
+            try:
+                food_input = input("Food format or timer range is incorrect. Please try again:\n")
+                temp_food = int(food_input)
+                while not temp_food in range(3, 61):
+                    print("  ... Food buff timer needs to be between 3 to 60.\n")
+                food_lock += 1
+            except ValueError:
+                print("  ... Food buff timer must be an integer.\n")
+
+        while pot_lock == 1:
+            try:
+                pot_input = input("Pot format or timer range is incorrect. Please try again:\n")
+                temp_pot = int(pot_input)
+                while not temp_pot in range(3, 16):
+                    print("  ... Pot buff timer needs to be between 3 to 15.\n")
+                pot_lock += 1
+            except ValueError:
+                print("  ... Pot buff timer must be an integer.\n")
+
+        user_input[0] = int(temp_food)
+        user_input[1] = int(temp_pot)
 
         return user_input
 
