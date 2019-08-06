20f789a3e0589646e1a293aaa813f91d4f2746cf
kai-dg <haruspace.dev@gmail.com>
2019-08-06 05:41:18 -0700

    added collectable mode

--- ffxiv_jidoucraft.py
+++ ffxiv_jidoucraft.py
@@ -6,7 +6,7 @@ from process import Process
 from time import sleep
 
 def args_checker():
-    accepted_args = ["foodbuff", "potbuff"]
+    accepted_args = ["foodbuff", "potbuff", "collectable"]
     args = sys.argv[1:]
     all_ok = 0
     for arg in args:
@@ -32,6 +32,8 @@ if __name__ == "__main__":
     pot_time = None
     pot_limiter = 0
 
+    collectable = 0
+
     if "foodbuff" in args:
         print("Adding foodbuffer to crafting automation...")
         food_time = input("What is your current food buff time?\n")
@@ -40,10 +42,15 @@ if __name__ == "__main__":
         print("Adding potbuffer to crafting automation...")
         pot_time = input("What is your current pot buff time?\n")
         potbuff = 1
+    if "collectable" in args:
+        print("Collectable mode activated...")
+        collectable = 1
 
     # Regular auto-craft
     print("Starting crafting automation...")
-    print("TO QUIT: PRESS CTRL+C\n")
+    print("TO QUIT: PRESS CTRL+C")
+    print("\nMake sure your MOUSE CURSOR is HIGHLIGHTING THE CRAFT ITEM BEFORE MINIMIZING\n")
+    print("\nMake sure your MOUSE CURSOR is NOT HIGHLIGHTING ANYTHING BEFORE MINIMIZING\n")
     while True:
         if foodbuff == 1:
             if food_limiter >= food_time:
@@ -78,11 +85,19 @@ if __name__ == "__main__":
             just_buffed = 0
 
         print("  -> Pressing + Selecting 'Synthesis'")
-        for zero_counter in range(25):
+        for zero_counter in range(20):
             ffxiv.press_key("{VK_NUMPAD0}")
-        sleep(5)
-        food_limiter += 7
-        pot_limiter += 7
+        sleep(2)
+        food_limiter += 5
+        pot_limiter += 5
+
+        if collectable == 1:
+            print("  -> Collectable mode selected, please wait.")
+            for zero_counter in range(15):
+                ffxiv.press_key("{VK_NUMPAD0}")
+            sleep(2)
+            food_limiter += 4
+            pot_limiter += 4
 
         ffxiv.press_key(json_data["k1"])
         print("  -> Pressing Macro 1")
