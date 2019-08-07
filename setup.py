a9fdf87222ecebbcbfb204cd68a641c447083581
kai-dg <haruspace.dev@gmail.com>
2019-08-07 04:31:48 -0700

    added macro amount for macro pushes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import json
 abs_path = os.path.dirname(os.path.abspath(__file__))
 json_file_name = "settings.json"
 json_file_path = abs_path + "\\" + json_file_name
-json_template = {"m1": 46, "m2": 46, "m3": 0, "m4": 0, "k1": "1", "k2": "2", "k3": "3", "k4": "4", "process_name": "ffxiv_dx11.exe", "food_key": "4", "pot_key": "5", "craft_key": "~"}
+json_template = {"m1": 46, "m2": 46, "m3": 0, "m4": 0, "k1": "1", "k2": "2", "k3": "3", "k4": "4", "process_name": "ffxiv_dx11.exe", "food_key": "4", "pot_key": "5", "craft_key": "~", "macro_amount": 2}
 
 def setup():
     print("Setting up the crafting automater for FFXIV...")
