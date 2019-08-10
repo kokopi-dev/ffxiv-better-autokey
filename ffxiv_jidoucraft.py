8a29a119817436809cfcce0bf9c3c638d71f9763
kai-dg <haruspace.dev@gmail.com>
2019-08-10 04:14:13 -0700

    added a notification command

--- ffxiv_jidoucraft.py
+++ ffxiv_jidoucraft.py
@@ -4,9 +4,14 @@ import sys
 from json_editor import json_reader
 from process import Process
 from time import sleep
+from win10toast import ToastNotifier
+
+def notifier(title=None, message=None):
+    toaster = ToastNotifier()
+    toaster.show_toast(title, message, duration=7)
 
 def args_checker():
-    accepted_args = ["foodbuff", "potbuff", "collectable", "counter"]
+    accepted_args = ["foodbuff", "potbuff", "collectable", "limit", "notify", "--help"]
     args = sys.argv[1:]
     all_ok = 0
     for arg in args:
@@ -27,16 +32,16 @@ def int_validator(value=None):
         return value
 
 if __name__ == "__main__":
-    if sys.argv[1] == "--help":
+    json_data = json_reader()
+    ffxiv = Process(json_data["process_name"])
+
+    args = args_checker()
+    if "--help" in args:
         with open("help.txt", "r") as f:
             helper = f.read()
             print(helper)
         quit()
 
-    json_data = json_reader()
-    ffxiv = Process(json_data["process_name"])
-
-    args = args_checker()
     just_buffed = 0
 
     foodbuff = 0
@@ -49,9 +54,17 @@ if __name__ == "__main__":
 
     collectable = 0
 
-    craft_limiter = 0
     craft_counter = 0
+    craft_amount = None
 
+    macro_amount = json_data["macro_amount"]
+    timer_list = ["m1", "m2", "m3", "m4"]
+    button_list = ["k1", "k2", "k3", "k4"]
+
+    notify = 0
+
+    if "notify" in args:
+        notify = 1
     if "foodbuff" in args:
         print("Adding foodbuffer to crafting automation...")
         food_time = input("What is your current food buff time?\n")
@@ -65,7 +78,7 @@ if __name__ == "__main__":
     if "collectable" in args:
         print("Collectable mode activated...")
         collectable = 1
-    if "counter" in args:
+    if "limit" in args:
         print("How many crafts do you want to limit to?")
         craft_amount = input()
         try:
@@ -73,7 +86,12 @@ if __name__ == "__main__":
         except ValueError:
             print("  -> ERROR: Enter a number. Run ffxiv_jidoucraft.py again.")
             quit()
-        craft_limiter = 1
+        m_time = 0
+        for i in range(macro_amount):
+            m_time += json_data[timer_list[i]]
+            m_time += 8 # Numpad 0 presses estimated amount of time it takes
+        res = (craft_amount * m_time) / 60
+        print("  -> Estimated completion time: {:.2f} minutes.".format(res))
 
     # Regular auto-craft
     print("Starting crafting automation...")
@@ -81,6 +99,15 @@ if __name__ == "__main__":
     print("\nMake sure your MOUSE CURSOR is HIGHLIGHTING THE CRAFT ITEM BEFORE MINIMIZING\n")
     print("\nMake sure your MOUSE CURSOR is NOT HIGHLIGHTING ANYTHING BEFORE MINIMIZING\n")
     while True:
+        if "limit" in args:
+            print("Craft #{}".format(craft_counter + 1))
+            if craft_counter >= craft_amount:
+                print("Crafted {} times, quitting program.".format(craft_amount))
+                if notify == 1:
+                    notify_message = "Crafted {} times.".format(craft_amount)
+                    notifier("Crafting Batch Finished", notify_message)
+                quit()
+
         if foodbuff == 1:
             if food_limiter >= food_time:
                 print("  -> Standing up by pressing ESC")
@@ -114,11 +141,11 @@ if __name__ == "__main__":
             just_buffed = 0
 
         print("  -> Pressing + Selecting 'Synthesis'")
-        for zero_counter in range(20):
+        for zero_counter in range(10):
             ffxiv.press_key("{VK_NUMPAD0}")
         sleep(2)
-        food_limiter += 5
-        pot_limiter += 5
+        food_limiter += 4
+        pot_limiter += 4
 
         if collectable == 1:
             print("  -> Collectable mode selected, please wait.")
@@ -128,9 +155,6 @@ if __name__ == "__main__":
             food_limiter += 4
             pot_limiter += 4
 
-        macro_amount = json_data["macro_amount"]
-        timer_list = ["m1", "m2", "m3", "m4"]
-        button_list = ["k1", "k2", "k3", "k4"]
         for i in range(macro_amount):
             ffxiv.press_key(json_data[button_list[i]])
             print("  -> Pressing Macro {}".format(i + 1))
@@ -142,9 +166,4 @@ if __name__ == "__main__":
         sleep(3)
         food_limiter += 3
         pot_limiter += 3
-
-        if craft_limiter == 1:
-            craft_counter += 1
-            if craft_counter >= craft_amount:
-                print("Crafted {} times, quitting program.".format(craft_amount))
-                quit()
+        craft_counter += 1
