25076225c998af18618d616d706e7f7d616c06df
kai-dg <haruspace.dev@gmail.com>
2019-09-02 04:16:06 -0700

    updated for flask

--- crafter.py
+++ crafter.py
@@ -16,39 +16,60 @@ class AutoCraft:
         self.opt_limit = 0
         self.opt_notify = 0
         self.opt_repair = 0
-        # Used for Flask if 2nd arg is 'Flask'
         self.flask = 0
-        if mode == "Flask":
-            self.flask = 1
-            # self.macro_profiles = json_reader("/macro_profiles/macro_profiles.json")
+        # if flask becomes 1, use bottom 3
+        self.food_time = 0
+        self.craft_amount = 0
+        self.pot_time = 0
         self.json_data = json_data
         if self.json_data:
             self.ffxiv = Process(self.json_data["process_name"])
             self.macro_amount = self.json_data["macro_amount"]
             self.timer_list = ["m1", "m2", "m3", "m4"]
             self.button_list = ["k1", "k2", "k3", "k4"]
-        self.crafter()
+        else:
+            print("ERROR: JSON file not found. Try running setup.py")
+        if self.flask == 0:
+            self.crafter()
 
     def args_checker(self):
         accepted_args = ["repair", "foodbuff", "potbuff", "collectable", "limit", "notify", "--help"]
         args = sys.argv[1:]
         all_ok = 0
+        if "flask" in set(args):
+            self.flask = 1
+            try:
+                with open("automation_settings.json", "r") as f:
+                    data = json.load(f)
+                    self.food_time = data["temp_food_time"]
+                    self.craft_amount = data["temp_craft_amount"]
+                    self.pot_time = data["temp_pot_time"]
+            except FileNotFoundError:
+                print("ERROR: Requires automation_settings.json")
+                quit()
         for arg in args:
-            if arg in accepted_args:
-                if arg == "--help":
-                    self.opt_help = 1
-                if arg == "repair":
-                    self.opt_repair = 1
-                if arg == "foodbuff":
-                    self.opt_foodbuff = 1
-                if arg == "potbuff":
-                    self.opt_potbuff = 1
-                if arg == "collectable":
-                    self.opt_collectable = 1
-                if arg == "limit":
-                    self.opt_limit = 1
-                if arg == "notify":
-                    self.opt_notify = 1
+            if arg == "flask":
+                all_ok += 1
+            if arg == "--help":
+                self.opt_help = 1
+                all_ok += 1
+            if arg == "repair":
+                self.opt_repair = 1
+                all_ok += 1
+            if arg == "foodbuff":
+                self.opt_foodbuff = 1
+                all_ok += 1
+            if arg == "potbuff":
+                self.opt_potbuff = 1
+                all_ok += 1
+            if arg == "collectable":
+                self.opt_collectable = 1
+                all_ok += 1
+            if arg == "limit":
+                self.opt_limit = 1
+                all_ok += 1
+            if arg == "notify":
+                self.opt_notify = 1
                 all_ok += 1
         if all_ok != len(args):
             print("Incorrect argument(s). Choose from this list:\n{}\n".format(accepted_args))
@@ -126,6 +147,7 @@ class AutoCraft:
             quit()
 
         just_buffed = 0
+        repair_limiter = 0
 
         foodbuff = 0
         food_time = None
@@ -140,32 +162,42 @@ class AutoCraft:
         craft_counter = 0
         craft_amount = None
 
-
         notify = 0
 
         if self.opt_notify == 1:
             notify = 1
         if self.opt_foodbuff == 1:
             print("Adding foodbuffer to crafting automation...")
-            food_time = input("What is your current food buff time?\n")
-            food_time = self.int_validator(food_time)
+            if self.flask == 1:
+                food_time = self.food_time
+            else:
+                food_time = input("What is your current food buff time?\n")
+                food_time = self.int_validator(food_time)
             foodbuff = 1
         if self.opt_potbuff == 1:
             print("Adding potbuffer to crafting automation...")
-            pot_time = input("What is your current pot buff time?\n")
-            pot_time = self.int_validator(pot_time)
+            if self.flask == 1:
+                pot_time = self.pot_time
+            else:
+                pot_time = input("What is your current pot buff time?\n")
+                pot_time = self.int_validator(pot_time)
             potbuff = 1
         if self.opt_collectable == 1:
             print("Collectable mode activated...")
         if self.opt_limit == 1:
             print("How many crafts do you want to limit to?")
-            craft_amount = input()
-            try:
-                craft_amount = int(craft_amount)
-            except ValueError:
-                print("  -> ERROR: Enter a number. Run ffxiv_jidoucraft.py again.")
-                quit()
+            if self.flask == 1:
+                craft_amount = self.craft_amount
+            else:
+                craft_amount = input()
+                try:
+                    craft_amount = int(craft_amount)
+                except ValueError:
+                    print("  -> ERROR: Enter a number. Run ffxiv_jidoucraft.py again.")
+                    quit()
             self.time_estimater(self.json_data, craft_amount)
+        if self.opt_repair == 1:
+            print("Auto repair activated...")
 
         # Regular auto-craft
         print("Starting crafting automation...")
@@ -175,6 +207,11 @@ class AutoCraft:
                 if craft_counter >= craft_amount:
                     self.auto_end(craft_counter, craft_amount)
                 print("Craft #{}".format(craft_counter + 1))
+
+            if self.opt_repair == 1:
+                if repair_limiter >= 2280:
+                    self.auto_repair()
+                    repair_limiter = 0
 
             if foodbuff == 1:
                 if food_limiter >= food_time:
@@ -202,6 +239,7 @@ class AutoCraft:
             sleep(0.5)
             food_limiter += 2
             pot_limiter += 2
+            repair_limiter += 2
 
             if self.opt_collectable == 1:
                 print("  -> Collectable mode selected, please wait.")
@@ -210,6 +248,7 @@ class AutoCraft:
                 sleep(0.5)
                 food_limiter += 2
                 pot_limiter += 2
+                repair_limiter += 2
 
             for i in range(self.macro_amount):
                 self.ffxiv.press_key(self.json_data[self.button_list[i]])
@@ -218,10 +257,12 @@ class AutoCraft:
                 sleep(self.json_data[self.timer_list[i]])
                 food_limiter += self.json_data[self.timer_list[i]]
                 pot_limiter += self.json_data[self.timer_list[i]]
+                repair_limiter += self.json_data[self.timer_list[i]]
 
             sleep(3)
             food_limiter += 3
             pot_limiter += 3
+            repair_limiter += 3
             craft_counter += 1
 
 if __name__ == "__main__":
